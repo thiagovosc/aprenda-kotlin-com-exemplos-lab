d0e54c22d2ab73f763bd400461e2d988ff2f20a4
Thiago de Jesus <87780843+thiagovosc@users.noreply.github.com>
2024-01-19 15:39:53 -0300

    Resultado Final

--- desafio.kt
+++ desafio.kt
@@ -1,42 +1,34 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
+
 enum class State { Basic, Intermediate, Advanced }
 
-fun main() {
-    val state = State.Basic
+  val state = State.Basic
     val message = when (state)
-    {                     
+    {
         State.Basic -> "Basic!"
         State.Intermediate -> "Intermediate!"
         State.Advanced -> "Advanced!" }
- 
-    println(message)
- 
-}
 
-class Usuario
+data class User(val name: String)
 
 data class EducationalContent(val name: String, val TimeStudying: Int) {          
     override fun equals(other: Any?) =
         other is EducationalContent && other.TimeStudying == this.TimeStudying             
-}
-fun main() {
-    val EducationalContent = EducationalContent("Philip", 24)
-    println(EducationalContent)                                          }
 
+data class Education(val name: String, var content: List<EducationalContent>) {
 
-
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+    val registered = mutableListOf<User>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun enroll(user: User) {
+        TODO("Utilize o parâmetro $user para simular uma matrícula (usar a lista de $registered).")
+      registered.add(user)
     }
-}
 
-fun main() {
+ {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+    var educationalContents = mutableListOf( EducationalContent("Strings", 10), EducationalContent("Collections", 20))
+    val education = Education("Kotlin", educationalContents)
+    var thiago = User("Thiago")
+   education.enroll(thiago)    }
