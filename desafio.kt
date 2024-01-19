4281fc971d24a56d40082d7ec683ea8df8efe82f
Thiago de Jesus <87780843+thiagovosc@users.noreply.github.com>
2024-01-19 17:15:26 -0300

    Representação final

--- desafio.kt
+++ desafio.kt
@@ -14,9 +14,9 @@ data class User(val name: String)
 
 data class EducationalContent(val name: String, val TimeStudying: Int) {          
     override fun equals(other: Any?) =
-        other is EducationalContent && other.TimeStudying == this.TimeStudying             
+        other is EducationalContent && other.TimeStudying == this.TimeStudying  }           
 
-data class Education(val name: String, var content: List<EducationalContent>) {
+data class Education(val name: String, var content: List<EducationalContent>) 
 
     val registered = mutableListOf<User>()
     
@@ -27,8 +27,10 @@ data class Education(val name: String, var content: List<EducationalContent>) {
 
  {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")}
+
+    fun main() {
     var educationalContents = mutableListOf( EducationalContent("Strings", 10), EducationalContent("Collections", 20))
     val education = Education("Kotlin", educationalContents)
     var thiago = User("Thiago")
-   education.enroll(thiago)    }
+   education.enroll(thiago)    } 
